19faf8b975fa22e8e6e968e2163068ffdcabeb7a
Kwizera Ibrahim <104641089+ibrahim703042@users.noreply.github.com>
2025-04-07 17:09:45 +0200

    cmt radix

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,21 +1,18 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./reducers/appReducer";
 import userReducer from "./reducers/userReducer";
 
-
 // Create a store with initial cart state from localStorage
 const store = configureStore({
-    reducer: {
-        user: userReducer,
-        app: appReducer,
-    },
-  
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false,
-        }),
+  reducer: {
+    user: userReducer,
+    app: appReducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
-
-
 
 export default store;

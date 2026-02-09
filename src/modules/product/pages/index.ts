588d3da9b026db23361dc4082c3bdb78a343af69
ibrahim274651 <ikwizera@asystresources.com>
2026-02-09 16:44:13 +0200

    cmt

--- src/modules/product/pages/index.ts
+++ src/modules/product/pages/index.ts
@@ -2,7 +2,7 @@ export { default as ProductDashboard } from "./ProductDashboard";
 export { default as CategoryPage } from "./CategoryPage";
 export { default as SubCategoryPage } from "./SubCategoryPage";
 export { default as BrandPage } from "./BrandPage";
-export { default as ProductListPage } from "./ProductListPage";
+export { default as ProductListPage } from "./products/ProductListPage";
 export { default as AttributePage } from "./AttributePage";
 export { default as TagPage } from "./TagPage";
 export { default as BundlePage } from "./BundlePage";
